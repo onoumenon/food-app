d62cf232c577f716838c0febf0ca2dfad1b8b1e0
Angeline <atan@thoughtworks.com>
2019-02-15 17:11:43 +0800

    Filter the restaurant list depending on cuisine selected on FilterBar
    - Dynamic render of FilterBar menu from cuisine service
    - handle filtering upon click

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,24 +1,45 @@
 import React, { Component } from "react";
 import { getRestaurants } from "../../services/restaurantService";
+import { getCuisines, getDefaultCuisine } from "../../services/cuisineService";
 import Restaurant from "../Restaurant/Restaurant";
 import FilterBar from "../FilterBar/FilterBar";
 import "./HomePage.scss";
 
 class HomePage extends Component {
   state = {
-    restaurants: getRestaurants()
+    restaurants: getRestaurants(),
+    cuisines: [getDefaultCuisine(), ...getCuisines()],
+    selectedCuisine: null
   };
+
+  handleCuisineSelect = cuisine => {
+    const finalCuisine = cuisine.name === "All" ? null : cuisine;
+    this.setState({
+      selectedCuisine: finalCuisine
+    });
+  };
+
   render() {
-    const { restaurants } = this.state;
+    const { restaurants, cuisines, selectedCuisine } = this.state;
+    const filteredRestaurantList =
+      selectedCuisine && selectedCuisine._id
+        ? restaurants.filter(res => res.cuisine._id === selectedCuisine._id)
+        : restaurants;
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-4 mx-auto mt-3">
-            <FilterBar />
+            <FilterBar
+              cuisines={cuisines}
+              selected={selectedCuisine}
+              handleClick={this.handleCuisineSelect}
+            />
           </div>
         </div>
+
         <div className="row">
-          {restaurants.map(restaurant => (
+          {filteredRestaurantList.map(restaurant => (
             <div className="card-col" key={restaurant._id}>
               <Restaurant details={restaurant} />
             </div>
