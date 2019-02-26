a6514b8a127be5b784aafbd40383165cf5c5454e
huitian <tanhuitian@gmail.com>
2019-02-26 16:01:09 +0800

    Implement selected button test for FilterBar

--- src/components/FilterBar/FilterBar.test.js
+++ src/components/FilterBar/FilterBar.test.js
@@ -4,21 +4,31 @@ import React from "react";
 import { render } from "react-testing-library";
 import FilterBar from "./FilterBar";
 
-test("renders cuisines from list", () => {
-  const data = [
-    {
-      _id: "5c3430ecfc13ae122d000000",
-      name: "Western"
-    },
-    {
-      _id: "5c3430ecfc13ae122d000001",
-      name: "Japanese"
-    }
-  ];
+const data = [
+  {
+    _id: "5c3430ecfc13ae122d000000",
+    name: "Western"
+  },
+  {
+    _id: "5c3430ecfc13ae122d000001",
+    name: "Japanese"
+  }
+];
 
+test("renders cuisines from list", () => {
   const { getByText } = render(<FilterBar cuisines={data} />);
   expect(getByText(/western/i)).toBeInTheDocument();
   expect(getByText(/japanese/i)).toBeInTheDocument();
 });
 
-test.skip("renders selectedCuisine with className active", () => {});
+test("renders selectedCuisine with className btn btn-outline-primary", () => {
+  const selectedCuisine = {
+    _id: "5c3430ecfc13ae122d000001",
+    name: "Japanese"
+  };
+  const { getByText, debug } = render(
+    <FilterBar cuisines={data} selected={selectedCuisine._id} />
+  );
+  expect(getByText(/japanese/i)).toHaveClass("btn btn-outline-primary");
+  debug();
+});
