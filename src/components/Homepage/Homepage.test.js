83f0bb689ae602a7ab576f3db481857612b92e70
huitian <tanhuitian@gmail.com>
2019-02-26 11:44:00 +0800

    Implement Test for Homepage compoment

--- src/components/Homepage/Homepage.test.js
+++ src/components/Homepage/Homepage.test.js
@@ -3,25 +3,40 @@ import "react-testing-library/cleanup-after-each";
 import React from "react";
 import { render } from "react-testing-library";
 import HomePage from "./HomePage";
-import * as RestaurantService from "../../services/restaurantService";
+import * as RestaurantService from "./../../services/restaurantService";
+
+const data = [
+  {
+    _id: "5c342ac9fc13ae39f8000000",
+    name: "The Burger Bar by Fatboy's Concepts (Boat Quay)",
+    openingTime: "11:00",
+    closingTime: "22:30",
+    cuisine: { name: "Western" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
+  },
+  {
+    _id: "5c342ac9fc13ae39f8000001",
+    name: "The Soup Spoon (Tanjong Pagar Exchange)",
+    openingTime: "12:00",
+    closingTime: "21:30",
+    cuisine: { name: "Western" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000001.jpg"
+  }
+];
+
+beforeEach(() => {
+  jest
+    .spyOn(RestaurantService, "getRestaurants")
+    .mockImplementation(() => data);
+});
+
+afterEach(() => {
+  RestaurantService.getRestaurants.mockRestore();
+});
 
 test("Homepage renders list of two Restaurants", () => {
-  const data = [
-    {
-      _id: "5c342ac9fc13ae39f8000000",
-      name: "The Burger Bar by Fatboy's Concepts (Boat Quay)",
-      openingTime: "11:00",
-      closingTime: "22:30",
-      cuisine: { name: "Western" },
-      imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
-    },
-    {
-      _id: "5c342ac9fc13ae39f8000001",
-      name: "The Soup Spoon (Tanjong Pagar Exchange)",
-      openingTime: "12:00",
-      closingTime: "21:30",
-      cuisine: { name: "Western" },
-      imageUrl: "images/restaurants/5c342ac9fc13ae39f8000001.jpg"
-    }
-  ];
+  const { getAllByText } = render(<HomePage />);
+
+  expect(RestaurantService.getRestaurants).toHaveBeenCalledTimes(1);
+  expect(getAllByText("Order").length).toEqual(2);
 });
